357ded21ca41b18053eae93e714cfe49b4290156
AdrianaKaella <adriana.borger@telfort.nl>
2017-03-01 13:59:59 +0100

    Les 1 opdracht 1

---
+++ Les 1/Les1_Opdr1_Particles/src/ofApp.cpp
@@ -0,0 +1,20 @@
+#include "ofApp.h"
+
+
+void ofApp::setup(){
+	//Path cirkel formaat aangeven.
+}
+
+
+void ofApp::update(){
+	//circleRandom random beweging geven.
+	//circlePath rand van Path laten volgen (ofzo).
+}
+
+
+void ofApp::draw(){
+	//Path tekenen.
+
+	//circleRandom random aantal keer laten spawnen en na bepaalde tijd laten verdwijnen.
+	//circlePath random aantal keer laten spawnen en na bepaalde tijd laten verdwijnen.
+}
